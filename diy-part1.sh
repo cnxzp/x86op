72cdf826be31bc740ef4c3ddee27d58b98916a6b
cnxzp <xuzepeng@zoho.com>
2022-08-19 11:10:36 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,6 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
-#echo 'src-git adguard https://github.com/yang229/luci-app-adguardhome' >>feeds.conf.default
+echo 'src-git adguard https://github.com/yang229/luci-app-adguardhome' >>feeds.conf.default
